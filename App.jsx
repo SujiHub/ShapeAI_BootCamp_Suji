5a116c9bb2d79dfa829196a3cff24b605447313c
SujiHub <85443800+SujiHub@users.noreply.github.com>
2021-06-06 22:28:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Bodie from "./Bodie.jsx"
+function App(){
+    return(
+        <div>
+            <Header />
+            <Footer />
+            <Bodie />
+            <Bodie /><Bodie /><Bodie /><Bodie /><Bodie />
+        </div>
+
+    );
+}
+export default App;
---
+++ Bodie.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Bodie(){
+    
+    return(
+        <div  className="bodie">
+            <h1>CONTENT</h1>
+            <p>
+            Music is a pleasant sound which is a combination of melodies and harmony and which soothes you. Music may also refer to the art of composing such pleasant sounds with the help of the various musical instruments.
+            </p>
+        </div>
+
+    );
+}
+export default Bodie;
